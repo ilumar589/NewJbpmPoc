8d25b452ff888612f3df85e0a708c9c99fb420eb
Parvu Eduard <parvueduard@gmail.com>
2019-09-07 11:26:02 +0300

    - initial commit

---
+++ src/main/java/com/poc/jbpm/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.poc.jbpm.config.audit;
---
+++ src/main/java/com/poc/jbpm/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.poc.jbpm.repository;
+
+import com.poc.jbpm.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
